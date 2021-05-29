f8cfe2e117883fe1c1fdc1135fb94079d577dbed
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-05-29 12:58:00 +0600

    реализовал первый этап!

--- source/galapp/admin.py
+++ source/galapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from galapp.models import Albom, Photos
+
+
+class PhotosAdmin(admin.ModelAdmin):
+    list_display = ['id', 'foto', 'text', 'created_at', 'user']
+    list_filter = ['text']
+    search_fields = ['text']
+    fields = ['id', 'text', 'foto', 'user', 'is_private', 'album', 'created_at' ]
+    readonly_fields = ['id', 'created_at']
+
+admin.site.register(Photos, PhotosAdmin)
+admin.site.register(Albom)

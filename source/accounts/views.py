432f6ba2fc7aead8fdfc67cd90b18cc354ee8f99
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-05-29 19:48:02 +0600

    создал проект аккаунта и реализовал авторизацию вход и выход

---
+++ source/accounts/views.py
@@ -0,0 +1,14 @@
+from accounts.forms import UserRegisterForm
+from django.shortcuts import render, redirect
+
+
+def register_view(request, *args, **kwargs):
+    context = {}
+    form = UserRegisterForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index_project')
+    context['form'] = form
+    return render(request, 'registration/registe.html', context=context)
